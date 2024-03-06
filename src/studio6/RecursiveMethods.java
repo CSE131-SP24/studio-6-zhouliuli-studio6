a338ae1077313f6a39ec5a40c2e5579873f1e6f3
lzy03 <lzy03@Aero_2021.dhcp.wustl.edu>
2024-03-06 17:24:38 -0600

    Mar 6 Studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,15 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+
+		// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0)
 			return 0;
-		
+		else {
+			double i = 1/Math.pow(2,n);
+			return i + geometricSum(n-1);
+		}
+
 	}
 
 	/**
@@ -27,25 +32,52 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
+
+		// FIXME compute the gcd of p and q using recursion
+		if (p == 0 || q == 0)
 			return 0;
-		
+		else if (p%q != 0) {
+			int temp = q;
+			q = p % q;
+			p = temp;
+			return gcd(p, q);
+		}
+		else {
+			return q;
+		}
+
 	}
 
-	
+
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
 	 * 
 	 * @param array the array to create a reverse of, not to be mutated
+	 * @return 
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
+
+		// FIXME create a helper method that can recursively reverse the given array
+		if (array.length == 0)
 			return new int[0];
-		
+		else if (array.length == 1)
+			return array;
+		else {
+			int[] reversed = new int[array.length];
+			for(int i = 0; i <= array.length-1; i++) 
+				reversed[i] = array[i];
+			return reversed;
+		}
+	}
+	public static int index(int i) {
+		if (i == 0)
+			return 0;
+		else {
+			i += 1;
+			return index(i);
+		}
 	}
 
 	/**
@@ -59,7 +91,7 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
+
 		// FIXME
 	}
 
